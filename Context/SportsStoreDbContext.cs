43b0dadd11e59c055cdac185f061ab149e55f302
michaelmburu <ichangaimichael@yahoo.com>
2021-04-17 11:09:54 +0300

    Add project files.

---
+++ Context/SportsStoreDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Context
+{
+    public class SportsStoreDbContext : DbContext
+    {
+        public SportsStoreDbContext(DbContextOptions<SportsStoreDbContext> sportsStoreDbContextOptions) : base(sportsStoreDbContextOptions)
+        {
+           
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
